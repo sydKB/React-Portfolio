536eeefa5cd4cafec9de251398c3708fe70275d9
sydKB <sydneykb11@icloud.com>
2023-08-30 04:41:57 -0700

    rel attr

--- portfolio-app/src/components/Footer.js
+++ portfolio-app/src/components/Footer.js
@@ -5,13 +5,13 @@ import X from '../assets/Twitter.png'
 export default function Footer() {
   return (
     <footer className='footer'>
-        <a href='https://github.com/sydKB' target="_blank">
+        <a href='https://github.com/sydKB' rel="icon" target="_blank">
           <img src='https://www.shareicon.net/data/48x48/2015/09/02/94536_mark_512x512.png' alt='github' />
         </a>
-        <a href='https://www.linkedin.com/in/sydney-birza-106987185/' target="_blank">
+        <a href='https://www.linkedin.com/in/sydney-birza-106987185/' rel="icon" target="_blank">
           <img src='https://www.shareicon.net/data/48x48/2017/06/28/888041_logo_512x512.png' alt='linked-in' />
         </a>
-        <a href='https://twitter.com/sydb24' target="_blank">
+        <a href='https://twitter.com/sydb24' rel="icon" target="_blank">
           <img src={X} alt='twitter' />
         </a>
     </footer>
