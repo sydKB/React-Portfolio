fc3c67be27228c5b39c47865fd66942b4ccd8371
sydKB <sydneykb11@icloud.com>
2023-08-29 17:31:32 -0700

    more portfolio styling

--- portfolio-app/src/components/Portfolio.js
+++ portfolio-app/src/components/Portfolio.js
@@ -6,44 +6,49 @@ export default function Portfolio() {
     return (
         <section>
             <div className='portfolio'>
-                <div><h2>My Portfolio</h2></div>
-                <div className='project'>
-                    <a href="https://github.com/sydKB/To-Do-List" target="_blank">
-                        <img src= {ToDo} alt="To-Do List Project" />
-                        <div className='proj-description'>
-                            <p>To-Do List that stores items in local storage.</p>
-                        </div>
-                    </a>
-                    <a href="https://github.com/sydKB/To-Do-List" target="_blank">
-                        <img src= {ToDo} alt="ToDo" />
-                        <div className='proj-description'>
-                            <p>To-Do</p>
-                        </div>
-                    </a>
-                    <a href="https://github.com/sydKB/To-Do-List" target="_blank">
-                        <img src= {ToDo} alt="ToDo" />
-                        <div className='proj-description'>
-                            <p>To-Do</p>
-                        </div>
-                    </a>
-                    <a href="https://github.com/sydKB/To-Do-List" target="_blank">
-                        <img src= {ToDo} alt="ToDo" />
-                        <div className='proj-description'>
-                            <p>To-Do</p>
-                        </div>
-                    </a>
-                    <a href="https://github.com/sydKB/To-Do-List" target="_blank">
-                        <img src= {ToDo} alt="ToDo" />
-                        <div className='proj-description'>
-                            <p>To-Do</p>
-                        </div>
-                    </a>
-                    <a href="https://github.com/sydKB/To-Do-List" target="_blank">
-                        <img src= {ToDo} alt="ToDo" />
-                        <div className='proj-description'>
-                            <p>To-Do</p>
-                        </div>
-                    </a>
+                <div>
+                    <h2>My Portfolio</h2>
+                    <p>Hover over the project cards to read their description.</p>
+                    <p>Click them to go to their respective repositories in another tab.</p>
+                    <p>- - -</p>
+                </div>
+                <div className='projects'>
+                    <div className='project-image'>
+                        <div className='project-title'><h2>To Do List</h2></div>
+                        <a href="https://github.com/sydKB/To-Do-List" target="_blank">
+                            <img src= {ToDo} alt="To-Do List Project" />
+                            <div className='project-description'>
+                                <p>To-Do List that stores items in local storage.</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div className='image'>
+                        <div className='project-title'><h2>To Do List</h2></div>
+                        <a href="https://github.com/sydKB/To-Do-List" target="_blank">
+                            <img src= {ToDo} alt="To-Do List Project" />
+                            <div className='project-description'>
+                                <p>To-Do List that stores items in local storage.</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div className='image'>
+                        <div className='project-title'><h2>To Do List</h2></div>
+                        <a href="https://github.com/sydKB/To-Do-List" target="_blank">
+                            <img src= {ToDo} alt="To-Do List Project" />
+                            <div className='project-description'>
+                                <p>To-Do List that stores items in local storage.</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div className='image'>
+                        <div className='project-title'><h2>To Do List</h2></div>
+                        <a href="https://github.com/sydKB/To-Do-List" target="_blank">
+                            <img src= {ToDo} alt="To-Do List Project" />
+                            <div className='project-description'>
+                                <p>To-Do List that stores items in local storage.</p>
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
