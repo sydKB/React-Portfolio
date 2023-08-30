2d37869105568d3e9ecd14655730778fc864bc4f
sydKB <sydneykb11@icloud.com>
2023-08-29 18:14:50 -0700

    about me styling'

--- portfolio-app/src/components/AboutMe.js
+++ portfolio-app/src/components/AboutMe.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import '../pages.css'
+import ToDo from '../assets/ToDo.png'
 
 export default function AboutMe() {
   return (
     <section className='page'>
-    <div className='about'>
-      <h1>Sydney Birza</h1>
-      <p>sydneykb11@icloud.com</p>
-      <h2>Skills</h2>
-      <ul>
-        <li>this</li>
-        <li>this</li>
-        <li>this</li>
-        <li>this</li>
-        <li>this</li>
-        <li>this</li>
-        <li>this</li>
-        <li>this</li>
-      </ul>
-    </div>
+      <div className='about'>
+        <div className='me'>
+          <h1>Sydney Birza</h1>
+          <p>Full-Stack Developer</p>
+          <img src= {ToDo} alt="To-Do List Project" className='me-img'/>
+        </div>
+        <div className='bio'>
+          <h3>About me</h3>
+          <p>Hey! I'm Sydney, a UC Berkeley Full-Stack Coding bootcamp graduate.
+            This React portfolio should give you an idea of the skills I'ved acquired 
+            over the course of this program. Feel free to navigate between the tabs in
+            the navbar. 
+          </p>
+        </div>
+      </div>
+      
   </section>
   );
 };
