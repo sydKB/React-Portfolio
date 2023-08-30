5f81f5e06df775623ef5c746f5b41d83a871f4cb
sydKB <sydneykb11@icloud.com>
2023-08-29 17:04:09 -0700

    portfolio outline

--- portfolio-app/src/components/AboutMe.js
+++ portfolio-app/src/components/AboutMe.js
@@ -4,7 +4,21 @@ import '../pages.css'
 export default function AboutMe() {
   return (
     <section className='page'>
-      <p>my intro goes here</p>
-    </section>
+    <div className='about'>
+      <h1>Sydney Birza</h1>
+      <p>sydneykb11@icloud.com</p>
+      <h2>Skills</h2>
+      <ul>
+        <li>this</li>
+        <li>this</li>
+        <li>this</li>
+        <li>this</li>
+        <li>this</li>
+        <li>this</li>
+        <li>this</li>
+        <li>this</li>
+      </ul>
+    </div>
+  </section>
   );
 };
--- portfolio-app/src/components/Portfolio.js
+++ portfolio-app/src/components/Portfolio.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import { useState } from 'react';
 import '../pages.css'
+import ToDo from '../assets/ToDo.png';
 
 export default function Portfolio() {
     return (
-        <section className='page'>
-            <p>portfolio goes here</p>
+        <section>
+            <div className='portfolio'>
+                <div><h2>My Portfolio</h2></div>
+                <div className='project'>
+                    <a href="https://github.com/sydKB/To-Do-List" target="_blank">
+                        <img src= {ToDo} alt="To-Do List Project" />
+                        <div className='proj-description'>
+                            <p>To-Do List that stores items in local storage.</p>
+                        </div>
+                    </a>
+                    <a href="https://github.com/sydKB/To-Do-List" target="_blank">
+                        <img src= {ToDo} alt="ToDo" />
+                        <div className='proj-description'>
+                            <p>To-Do</p>
+                        </div>
+                    </a>
+                    <a href="https://github.com/sydKB/To-Do-List" target="_blank">
+                        <img src= {ToDo} alt="ToDo" />
+                        <div className='proj-description'>
+                            <p>To-Do</p>
+                        </div>
+                    </a>
+                    <a href="https://github.com/sydKB/To-Do-List" target="_blank">
+                        <img src= {ToDo} alt="ToDo" />
+                        <div className='proj-description'>
+                            <p>To-Do</p>
+                        </div>
+                    </a>
+                    <a href="https://github.com/sydKB/To-Do-List" target="_blank">
+                        <img src= {ToDo} alt="ToDo" />
+                        <div className='proj-description'>
+                            <p>To-Do</p>
+                        </div>
+                    </a>
+                    <a href="https://github.com/sydKB/To-Do-List" target="_blank">
+                        <img src= {ToDo} alt="ToDo" />
+                        <div className='proj-description'>
+                            <p>To-Do</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
         </section>
+        
     );
 };
