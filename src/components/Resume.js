92bac4f420293824e5158f897f6924190ab5fd36
Sydney Birza <127246601+sydKB@users.noreply.github.com>
2023-08-31 17:11:09 -0700

    file fix

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../pages.css'
-import '../assets/Resume.pdf'
+import '../assets/Resume.txt'
 
 export default function Resume() {
   return (
     <section className='page'>
       <div className='resume'>
         <h1>Sydney Birza</h1>
-        <a href="../assets/Resume.pdf" download="Resume.pdf">Click here to download</a>
+        <a href="../assets/Resume.txt" download="Resume.txt">Click here to download</a>
         <h2>Front-end Skills</h2>
         <ul>
           <li>HTML</li>
